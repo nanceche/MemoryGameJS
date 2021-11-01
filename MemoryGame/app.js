7b8a3256bc184ba585a16c9871b48d056be124c3
Your Name <you@example.com>
2021-11-01 14:53:08 +0000

    shuffle array

--- MemoryGame/app.js
+++ MemoryGame/app.js
@@ -9,22 +9,29 @@ playerLivesCount.innerHTML = playerLives;
 //Get data
 const getData = () => [
   { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/battersea=power.jpeg", name: "baker" },
+  { imgSrc: "./img/battersea=power.jpeg", name: "battersea" },
+  { imgSrc: "./img/camden.jpeg", name: "camden" },
+  { imgSrc: "./img/euston.jpeg", name: "euston" },
+  { imgSrc: "./img/il_1588xN.2968613192_onc4.jpg", name: "paddington" },
+  { imgSrc: "./img/lbridge.jpeg", name: "lbridge" },
+  { imgSrc: "./img/leicester-square.jpeg", name: "leicester" },
+  { imgSrc: "./img/oxford.jpeg", name: "oxford" },
   { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
-  { imgSrc: "./img/baker.jpeg", name: "baker" },
+  { imgSrc: "./img/battersea=power.jpeg", name: "battersea" },
+  { imgSrc: "./img/camden.jpeg", name: "camden" },
+  { imgSrc: "./img/euston.jpeg", name: "euston" },
+  { imgSrc: "./img/il_1588xN.2968613192_onc4.jpg", name: "paddington" },
+  { imgSrc: "./img/lbridge.jpeg", name: "lbridge" },
+  { imgSrc: "./img/leicester-square.jpeg", name: "leicester" },
+  { imgSrc: "./img/oxford.jpeg", name: "oxford" },
 ];
 
-const data = getData();
+//Randomise card
+
+const randomize = () => {
+  const cardData = getData();
+  cardData.sort(() => Math.random() - 0.5);
+  console.log(cardData);
+};
+
+randomize();
